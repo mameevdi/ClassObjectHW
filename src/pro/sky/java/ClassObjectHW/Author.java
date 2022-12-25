b6ab10c7181c5d7252c78d7a010de7fedc7e1039
mameevdi <mameevs@gmail.com>
2022-12-25 15:42:13 +0300

    Проект создан!

---
+++ src/pro/sky/java/ClassObjectHW/Author.java
@@ -0,0 +1,19 @@
+package pro.sky.java.ClassObjectHW;
+
+public class Author { // создаём класс Author
+    private String name;
+    private String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+}
